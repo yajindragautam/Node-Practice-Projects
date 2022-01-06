113d0f3ae24b9c430f93221276a052daaebffb8d
Yajindra <yajindragtm@gmail.com>
2022-01-06 21:31:54 +0545

    Second Project Completed successfully

--- Flash Message in Ejs 2/app.js
+++ Flash Message in Ejs 2/app.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const flash = require("connect-flash");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
-const flash = require("connect-flash");
 
-// Initilize app
+// Init express App
 const app = express();
+
+// Port Number
+const port = process.env.PORT || 5000;
 
 // Parse JSON Data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Set Cookie Parser, Session and Flash
+// Use Static Files
+app.use(express.static("public"));
+
+// Set Cookie Parser, Session & Flash
 app.use(cookieParser("SecretStringForCookies"));
 app.use(
   session({
@@ -20,27 +26,66 @@ app.use(
     saveUninitialized: true,
   })
 );
-app.use(flash);
+app.use(flash());
+
+// Flash Message Middleware
+app.use((req, res, next) => {
+  res.locals.notice = req.session.notice;
+  delete req.session.notice;
+  next();
+});
 
 // Set View Engine - EJS
 app.set("view engine", "ejs");
 
-
-// Get - Home Page
+// Get- Home Page
 app.get("/", (req, res) => {
   try {
-    res.render("pages/index");
-    res.status(200).json({
-        message:'Fetched Successfully..!'
-    })
+    res.render("pages/register");
   } catch (error) {
     console.log(error);
   }
 });
 
-// PortNumber
-const port = process.env.PORT || 5000;
+// Post - Users
+app.post("/", (req, res) => {
+  try {
+    if (
+      req.body.name == "" ||
+      req.body.email == "" ||
+      req.body.password == "" ||
+      req.body.conformPass == ""
+    ) {
+      req.session.notice = {
+        type: "danger",
+        intro: "Empty Fields..!",
+        message: "Please Fill All Required Information",
+      };
+      res.redirect("/");
+    }
+    if (req.body.password != req.body.conformPass) {
+      req.session.notice = {
+        type: "danger",
+        intro: "Miss matched Password",
+        message: "Try Again..!",
+      };
+      res.redirect("/");
+    }
+    // User Login Success
+    req.session.notice = {
+      type: "success",
+      intro: "Registered Successfully",
+      message: "Plese Login",
+    };
+    res.redirect("/");
+    console.log(req.body.name, req.body.email, req.body.password);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Listening To Server
 app.listen(port, () => {
-  console.log(`Litening to port ${port}`);
-  console.log(`Click To Visit: http://127.0.0.1:${port}`);
+  console.log(`Listening To Port ${port}`);
+  console.log(`Click Here To Visit: http://127.0.0.1:${port}`);
 });
